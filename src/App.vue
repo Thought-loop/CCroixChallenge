7fce753816d5135210965cc5ed5fddc2ffc618cc
Thought-loop <nick.steinhouse@gmail.com>
2022-04-06 19:08:51 -0400

    1 file modified, 3 added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <nav> Navbar </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
