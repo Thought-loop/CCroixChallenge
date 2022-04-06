08742673e717fb75976ecb6d52d3b8367f699d3b
Thought-loop <nick.steinhouse@gmail.com>
2022-04-06 19:45:31 -0400

    1 file modified, 1 added, 1 deleted

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class = 'bar'>
-      <img src = 
+      <img v-bind:src="logo" />
       <h3> Current Connection - Offline </h3>
       <h3> Photos - 100 </h3>
       <h3> Last Upload - 1/2/2022 4:21p </h3>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import logo from "./img/logo.png"
 export default {
     name: 'status-bar'
 }
