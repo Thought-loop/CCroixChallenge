ba216144125ec25052c0d58046742bdf309e6d56
Nick Steinhouse <nick.steinhouse@gmail.com>
2022-04-06 21:39:02 -0400

    added logo and text to status bar

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,19 +1,61 @@
 <template>
   <div class = 'bar'>
-      <img v-bind:src="logo" />
-      <h3> Current Connection - Offline </h3>
-      <h3> Photos - 100 </h3>
-      <h3> Last Upload - 1/2/2022 4:21p </h3>
+      <img class = 'bar-logo' v-bind:src="logo" />
+      <div class = 'bar-status-area'>
+          <div class = "bar-connection-status">
+            <h3>Current Connection</h3>
+            <h3>Offline</h3>
+          </div>
+          <div class = "bar-photo-count">
+            <h3>Photos</h3>
+            <h3>100</h3>
+          </div>
+          <div class = "bar-upload-info">
+            <h3>Last upload xx/xx/xxxx x:xx</h3>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import logo from "./img/logo.png"
+
 export default {
-    name: 'status-bar'
+    name: 'status-bar',
+    data(){
+        return{
+            logo: 'https://uxwing.com/wp-content/themes/uxwing/download/32-video-photography-multimedia/image-upload.png'
+        }
+    }
 }
 </script>
 
 <style>
+    .bar{
+        display: flex;
+        justify-content: space-between;
+    }
 
+    .bar-logo{
+        height: 10rem;
+    }
+
+    .bar-status-area{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-areas: 
+            "connection count"
+            "upload upload";
+    }
+
+    .bar-connection-status{
+        grid-area: connection;
+    }
+
+    .bar-photo-count{
+        grid-area: count;
+    }
+
+    .bar-upload-info{
+        grid-area: upload;
+    }
 </style>
